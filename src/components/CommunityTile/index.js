ddeb93f1f27e772d50ddc1742385686dcb73cc40
Sam <hatem@clearrain.xyz>
2020-04-29 16:50:38 -0400

    removed future code from community tile

--- src/components/CommunityTile/index.js
+++ src/components/CommunityTile/index.js
@@ -3,16 +3,10 @@ import React from 'react'
 import styles from './index.module.css'
 
 const CommunityTile = ({ name }) => {
-  const switchCommunities = async () => {
-    const activeSpace = await window.box.openSpace(name)
-    window.activeSpace = activeSpace
-    console.log(activeSpace, 'ACTIVE COMMUNITY')
-  }
-
   return (
     <div
       className={styles.communityTileContainer}
-      onClick={switchCommunities}>
+    >
       {name}
     </div>
   )
