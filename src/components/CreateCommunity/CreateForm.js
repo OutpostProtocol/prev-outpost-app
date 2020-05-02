82a640c690b2bb334732762c4bb6c0f85db13b03
Sam <hatem@clearrain.xyz>
2020-05-02 14:18:10 -0400

    stashing initial changes for redux saga

--- src/components/CreateCommunity/CreateForm.js
+++ src/components/CreateCommunity/CreateForm.js
@@ -1,47 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 
 import styles from './index.module.css'
 import { COMMUNITIES } from '../../constants'
-import { ADD_COMMUNITY } from '../../redux/actionTypes'
+import { ADD_COMMUNITY_ASYNC } from '../../redux/actionTypes'
 
 const CreateForm = ({ handleClose }) => {
   const address = useSelector(state => state.ethers.provider.selectedAddress)
   const dispatch = useDispatch()
-
   const [name, setName] = useState('')
   const [abbr, setAbbr] = useState('')
-
-  const createCom = async () => {
-    const thread = await window.space.joinThread(name, {
-      firstModerator: address,
-      members: true
-    })
-
-    const community = {
-      name,
-      abbr,
-      address: thread.address
-    }
-
-    const communities = await window.space.public.get(COMMUNITIES)
-
-    if (communities) {
-      window.space.public.set(COMMUNITIES, [
-        ...communities,
-        community
-      ])
-    } else {
-      window.space.public.set(COMMUNITIES, [
-        community
-      ])
-    }
-
-    dispatch({ type: ADD_COMMUNITY, community })
-    handleClose()
-  }
 
   const handleName = (event) => {
     setName(event.target.value)
@@ -54,6 +24,20 @@ const CreateForm = ({ handleClose }) => {
     }
 
     setAbbr(event.target.value)
+  }
+
+  const createCom = async () => {
+    // validate fields
+
+    const community = {
+      name,
+      abbr,
+      moderatorAddress: address
+    }
+
+    dispatch({ type: ADD_COMMUNITY_ASYNC, community })
+
+    handleClose()
   }
 
   return (
